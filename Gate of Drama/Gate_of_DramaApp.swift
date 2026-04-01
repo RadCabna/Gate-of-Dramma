6ab64e2e0c51ac1634d545b52a4ecf2536b4299a
Radcabna <radcabna@gmail.com>
2026-04-01 18:27:16 +0300

    Initial Commit

---
+++ Gate of Drama/Gate_of_DramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gate_of_DramaApp.swift
+//  Gate of Drama
+//
+//  Created by Алкександр Степанов on 01.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Gate_of_DramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
